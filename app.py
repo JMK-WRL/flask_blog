977065ba83cf399040aa92a3acc81291e15656be
Jonathan Kyule <jonathankyule2@gmail.com>
2024-02-27 15:04:37 +0300

    Adding comments

--- app.py
+++ app.py
@@ -2,22 +2,25 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
+# Function to establish a database connection
 def get_db_connection():
     conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
+    conn.row_factory = sqlite3.Row  # Access rows as dictionaries instead of tuples
     return conn
 
+# Function to retrieve a post from the database by ID
 def get_post(post_id):
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchall()
     conn.close()
     if post is None:
-        abort(404)
+        abort(404)  # Raise a 404 error if the post is not found
     return post
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'BLAnkspace321################################'
 
+# Route for the homepage
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -25,12 +28,13 @@ def index():
     conn.close()
     return render_template('index.html', posts=posts)
 
-
+# Route for displaying a single post
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
 
+# Route for creating a new post
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -38,18 +42,18 @@ def create():
         content = request.form['content']
 
         if not title:
-            flash('Title is required!')
+            flash('Title is required!')  # Flash a message if the title is missing
         else:
             conn = get_db_connection()
             conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                          (title, content))
             conn.commit()
             conn.close()
-            return redirect(url_for('index'))
-                         
+            return redirect(url_for('index'))  # Redirect to the homepage after creating the post
+
     return render_template('create.html')
 
-
+# Route for editing an existing post
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
     post = get_post(id)
@@ -71,6 +75,7 @@ def edit(id):
 
     return render_template('edit.html', post=post)
 
+# Route for deleting a post
 @app.route('/<int:id>/delete', methods=['POST'])
 def delete(id):
     post = get_post(id)
@@ -78,5 +83,5 @@ def delete(id):
     conn.execute('DELETE FROM posts WHERE id = ?', (id,))
     conn.commit()
     conn.close()
-    flash('"{}" wa successfully deleted!'.format(post['title']))
+    flash('"{}" was successfully deleted!'.format(post['title']))
     return redirect(url_for('index'))
--- init_db.py
+++ init_db.py
@@ -1,20 +1,24 @@
 import sqlite3
 
+# Establish a connection to the SQLite database (creating it if it doesn't exist)
 connection = sqlite3.connect('database.db')
 
-
+# Open and execute the schema.sql file to create the necessary table(s)
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
+# Create a cursor to execute SQL commands
 cur = connection.cursor()
 
+# Insert sample data into the 'posts' table
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
-            )
+            ('First Post', 'Content for the first post'))
 
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
-            )
+            ('Second Post', 'Content for the second post'))
 
+# Commit the changes to the database
 connection.commit()
+
+# Close the database connection
 connection.close()
